eca958822576abf66463636224c5c8a289d82b91
abdullah00001 <abdullahbinomarchowdhury02@gmail.com>
2024-12-03 11:58:52 +0600

    #learned : input/outpu and datatypes

---
+++ C++ Recap/Variable,Data Type & Condition/datatypes.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /* ========================================
+    Data Type Integer
+    Who Take Maximum maximum 10 digits value;
+    ========================================= */
+
+    int a = 199999999 + 1;
+    cout << a << endl;
+
+    /* ========================================
+    Data Type long Long Integer
+    Who Take Maximum maximum 18 digits value;
+    ========================================= */
+
+    long long int b = 199999999999999999 + 1;
+    cout << b << endl;
+
+    /* ========================================
+    Data Type float
+    Who Take Maximum maximum 6 digits value;
+    ========================================= */
+
+    float c;
+    cin >> c;
+    cout << fixed << setprecision(6) << c << endl;
+
+    /* ========================================
+    Data Type float
+    Who Take Maximum maximum 12 digits value;
+    ========================================= */
+
+    double d;
+    cin >> d;
+    cout << fixed << setprecision(12) << d << endl;
+
+    /* ========================================
+    Data Type char
+    Who Take every kind of character in your keyboards;
+    ========================================= */
+
+    char e;
+    cin >> e;
+    cout << e << endl;
+
+    /* ========================================
+    Data Type bool
+    Who Take only true or false;
+    ========================================= */
+
+    bool flag;
+    cin >> flag;
+    cout << flag << endl;
+
+    return 0;
+}
---
+++ C++ Recap/Variable,Data Type & Condition/input_output.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /* =============================
+    using cin cpp take input
+    and using cout cpp give or print output
+    '>>' use for input and '<<' use for output
+    =============================== */
+    int a, b;
+    cin >> a >> b;
+    cout << a << " " << b << endl;
+    return 0;
+};
